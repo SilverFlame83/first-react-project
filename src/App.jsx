474f9d808389656efa371e264eedd19f2c0a3ac1
VelvetSyntax <imren_83@abv.bg>
2024-01-01 17:42:15 +0200

    reuseable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import reactImg from './assets/react-core-concepts.png'
+import reactImg from "./assets/react-core-concepts.png";
+import {CORE_CONCEPTS} from './data'
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -6,7 +7,7 @@ function genRandomInt(max) {
 }
 
 function Header() {
-  const description  = reactDescriptions[genRandomInt(2)];
+  const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
@@ -20,10 +21,32 @@ function Header() {
   );
 }
 
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header />
+      <section id="core-concepts">
+        <ul>
+          <CoreConcepts
+            title={CORE_CONCEPTS[0].title}
+            image={CORE_CONCEPTS[0].image}
+            description={CORE_CONCEPTS[0].description}
+          ></CoreConcepts>
+          <CoreConcepts {...CORE_CONCEPTS[1]}></CoreConcepts>
+          <CoreConcepts {...CORE_CONCEPTS[2]}></CoreConcepts>
+          <CoreConcepts {...CORE_CONCEPTS[3]}></CoreConcepts>
+        </ul>
+      </section>
       <main>
         <h2>Time to get started!</h2>
       </main>
