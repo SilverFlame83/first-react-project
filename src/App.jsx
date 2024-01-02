e9f3a463a5a2b491270fcdf9cb4abfc4d351b97d
VelvetSyntax <imren_83@abv.bg>
2024-01-02 18:50:59 +0200

    using chldren props

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,34 @@
-import Header from './components/Header/Header';
-import CoreConcepts from './components/CoreConcepts';
-import {CORE_CONCEPTS} from './data'
+import Header from "./components/Header/Header";
+import CoreConcepts from "./components/CoreConcepts";
+import { CORE_CONCEPTS } from "./data";
+import TabButtons from "./components/TabButtons";
 
 function App() {
   return (
     <div>
       <Header />
-      <section id="core-concepts">
-        <ul>
-          <CoreConcepts
-            title={CORE_CONCEPTS[0].title}
-            image={CORE_CONCEPTS[0].image}
-            description={CORE_CONCEPTS[0].description}
-          ></CoreConcepts>
-          <CoreConcepts {...CORE_CONCEPTS[1]}></CoreConcepts>
-          <CoreConcepts {...CORE_CONCEPTS[2]}></CoreConcepts>
-          <CoreConcepts {...CORE_CONCEPTS[3]}></CoreConcepts>
-        </ul>
-      </section>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcepts
+              title={CORE_CONCEPTS[0].title}
+              image={CORE_CONCEPTS[0].image}
+              description={CORE_CONCEPTS[0].description}
+            ></CoreConcepts>
+            <CoreConcepts {...CORE_CONCEPTS[1]}></CoreConcepts>
+            <CoreConcepts {...CORE_CONCEPTS[2]}></CoreConcepts>
+            <CoreConcepts {...CORE_CONCEPTS[3]}></CoreConcepts>
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButtons>Components</TabButtons>
+            <TabButtons>JSX</TabButtons>
+            <TabButtons>Props</TabButtons>
+            <TabButtons>State</TabButtons>
+          </menu>
+        </section>
       </main>
     </div>
   );
