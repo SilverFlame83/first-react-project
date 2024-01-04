d66b29c879619086991cf69caa32a0abdac7deff
VelvetSyntax <imren_83@abv.bg>
2024-01-04 12:07:42 +0200

    conditionally rendering content

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
-import {useState} from 'react';
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
 import { CORE_CONCEPTS } from "./data";
 import TabButtons from "./components/TabButtons";
-import { EXAMPLES } from './data';
+import { EXAMPLES } from "./data";
 
 function App() {
-  const [selectedTopic, useSelectedTopic] = useState('components')
-  
+  const [selectedTopic, useSelectedTopic] = useState();
+
   function selectHandler(selectedBtn) {
-    useSelectedTopic(selectedBtn)
+    useSelectedTopic(selectedBtn);
     console.log(selectedTopic);
+  }
+
+  let topic = <h2>Please select a topic!!!</h2>;
+
+  if (selectedTopic) {
+    topic = (
+      <div id="tab-content">
+        <h2>{EXAMPLES[selectedTopic].title}</h2>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
@@ -31,22 +45,19 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButtons onSelect={()=> selectHandler('components')}>Components</TabButtons>
-            <TabButtons onSelect={()=> selectHandler('jsx')}>JSX</TabButtons>
-            <TabButtons onSelect={()=> selectHandler('props')}>Props</TabButtons>
-            <TabButtons onSelect={()=> selectHandler('state')}>State</TabButtons>
+            <TabButtons onSelect={() => selectHandler("components")}>
+              Components
+            </TabButtons>
+            <TabButtons onSelect={() => selectHandler("jsx")}>JSX</TabButtons>
+            <TabButtons onSelect={() => selectHandler("props")}>
+              Props
+            </TabButtons>
+            <TabButtons onSelect={() => selectHandler("state")}>
+              State
+            </TabButtons>
           </menu>
+          {topic}
         </section>
-        <div id='tab-content'>
-          <h2>{EXAMPLES[selectedTopic].title}</h2>
-          <p>{EXAMPLES[selectedTopic].description}</p>
-          <pre>
-            <code>
-            {EXAMPLES[selectedTopic].code}
-            </code>
-          </pre>
-
-        </div>
       </main>
     </div>
   );
